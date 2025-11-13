2e8ef09d6175bcb983a3eba8baef93205e0cd948
すみsann <pc.smithfield@gmail.com>
2025-11-13 16:29:22 +0900

    ハンドラーの共通処理をhandlerUtils.tsにまとめた

---
+++ src/handlers/handlerUtils.ts
@@ -0,0 +1,13 @@
+    import { ButtonInteraction, ChatInputCommandInteraction } from "discord.js";
+
+/**
+ * インタラクションからユーザー情報を取得するヘルパー関数
+ * @param interaction - ボタンまたはコマンドのインタラクション
+ * @returns ユーザー名とアイコンURL
+ */
+export const getUserInfo = (interaction: ButtonInteraction | ChatInputCommandInteraction) => {
+  return {
+    username: interaction.user.username,
+    userIconUrl: interaction.user.avatarURL()
+  };
+};
